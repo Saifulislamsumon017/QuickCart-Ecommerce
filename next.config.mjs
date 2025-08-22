6664f8c0538ba39d065120e432beb84fe5631173
Saiful Islam <sumon.sis169@gmail.com>
2025-08-22 08:22:41 +0600

    home section done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['raw.githubusercontent.com'], // add your image host here
+  },
+};
 
 export default nextConfig;
---
+++ src/data/products.js
@@ -0,0 +1,124 @@
+const products = [
+  {
+    _id: '67a1f4e43f34a77b6dde9144',
+    name: 'Apple AirPods Pro 2nd gen',
+    description:
+      'Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.',
+    price: 499.99,
+    offerPrice: 399.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/k4dafzhwhgcn5tnoylrw.webp',
+    ],
+    category: 'Earphone',
+  },
+  {
+    _id: '67a1f52e3f34a77b6dde914a',
+    name: 'Bose QuietComfort 45',
+    description:
+      'The Bose QuietComfort 45 headphones are engineered for exceptional sound quality and unparalleled noise cancellation.',
+    price: 429.99,
+    offerPrice: 329.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/m16coelz8ivkk9f0nwrz.webp',
+    ],
+    category: 'Headphone',
+  },
+  {
+    _id: '67a1f5663f34a77b6dde914c',
+    name: 'Samsung Galaxy S23',
+    description:
+      'The Samsung Galaxy S23 offers an all-encompassing mobile experience with its advanced AMOLED display, vibrant visuals and smooth interactions.',
+    price: 899.99,
+    offerPrice: 799.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/xjd4eprpwqs7odbera1w.webp',
+    ],
+    category: 'Smartphone',
+  },
+  {
+    _id: '67a1f5993f34a77b6dde914e',
+    name: 'Garmin Venu 2',
+    description:
+      'The Garmin Venu 2 smartwatch blends advanced fitness tracking with sophisticated design, offering heart rate monitoring, GPS, and sleep tracking.',
+    price: 399.99,
+    offerPrice: 349.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/hdfi4u3fmprazpnrnaga.webp',
+    ],
+    category: 'Smartwatch',
+  },
+  {
+    _id: '67a1f5ef3f34a77b6dde9150',
+    name: 'PlayStation 5',
+    description:
+      'The PlayStation 5 takes gaming to the next level with ultra-HD graphics, a powerful 825GB SSD, and ray tracing technology for realistic visuals.',
+    price: 599.99,
+    offerPrice: 499.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/dd3l13vfoartrgbvkkh5.webp',
+    ],
+    category: 'Accessories',
+  },
+  {
+    _id: '67a1f70c3f34a77b6dde9156',
+    name: 'Canon EOS R5',
+    description:
+      'The Canon EOS R5 is a game-changing mirrorless camera with a 45MP full-frame sensor, offering ultra-high resolution and the ability to shoot 8K video.',
+    price: 4199.99,
+    offerPrice: 3899.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/r5h370zuujvrw461c6wy.webp',
+    ],
+    category: 'Camera',
+  },
+  {
+    _id: '67a1f7c93f34a77b6dde915a',
+    name: 'MacBook Pro 16',
+    description:
+      "The MacBook Pro 16, powered by Apple's M2 Pro chip, offers outstanding performance with 16GB RAM and a 512GB SSD.",
+    price: 2799.99,
+    offerPrice: 2499.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/rzri7kytphxalrm9rubd.webp',
+    ],
+    category: 'Laptop',
+  },
+  {
+    _id: '67a1f8363f34a77b6dde915c',
+    name: 'Sony WF-1000XM5',
+    description:
+      'Sony WF-1000XM5 true wireless earbuds deliver immersive sound with Hi-Res Audio and advanced noise cancellation technology.',
+    price: 349.99,
+    offerPrice: 299.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/e3zjaupyumdkladmytke.webp',
+    ],
+    category: 'Earphone',
+  },
+  {
+    _id: '67a1f85e3f34a77b6dde915e',
+    name: 'Samsung Projector 4k',
+    description:
+      'The Samsung 4K Projector offers an immersive cinematic experience with ultra-high-definition visuals and realistic color accuracy.',
+    price: 1699.99,
+    offerPrice: 1499.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/qqdcly8a8vkyciy9g0bw.webp',
+    ],
+    category: 'Accessories',
+  },
+  {
+    _id: '67a1fa4b3f34a77b6dde9166',
+    name: 'ASUS ROG Zephyrus G16',
+    description:
+      'The ASUS ROG Zephyrus G16 gaming laptop is powered by the Intel Core i9 processor and RTX 4070 GPU, delivering top-tier gaming performance.',
+    price: 2199.99,
+    offerPrice: 1999.99,
+    image: [
+      'https://raw.githubusercontent.com/avinashdm/gs-images/main/quickcart/wig1urqgnkeyp4t2rtso.webp',
+    ],
+    category: 'Laptop',
+  },
+];
+
+export default products;
